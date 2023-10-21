ec091d7acbe08cc397a3139ddbf144124fb30522
Kevin Leonard Soldevilla <soldevillakevinleonard@gmail.com>
2023-10-21 17:20:01 +0800

    fix config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,14 @@
 module.exports = {
+  pathPrefix: "/cametechgroup",
   plugins: [
     {
-      resolve: "gatsby-theme-portfolio-minimal",
+      resolve: "cametechgroup",
       options: {
-        siteUrl: "https://gatsby-starter-portfolio-minimal-theme.netlify.app/", // Used for sitemap generation
+        siteUrl: "https://www.cametechgroup/", // Used for sitemap generation
         manifestSettings: {
           favicon: "./content/images/favicon.png", // Path is relative to the root
-          siteName: "My Minimal Portfolio", // Used in manifest.json
-          shortName: "Portfolio", // Used in manifest.json
+          siteName: "CAME TechGroup", // Used in manifest.json
+          shortName: "CAME", // Used in manifest.json
           startUrl: "/", // Used in manifest.json
           backgroundColor: "#FFFFFF", // Used in manifest.json
           themeColor: "#000000", // Used in manifest.json
